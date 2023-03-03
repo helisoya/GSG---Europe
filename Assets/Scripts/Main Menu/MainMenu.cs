6b87c15fb1fea69524ba36fd6865b717fc065b8c
helisoya <oudot.julien@laposte.net>
2023-03-03 10:15:02 +0100

    Main Menu

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -8,30 +8,31 @@ public class MainMenu : MonoBehaviour
     public Image loading_fill;
     public Image loading_back;
 
-    public Button start_button;
-
-    public Button quit;
+    public GameObject buttonsParent;
 
 
-    IEnumerator Load(){
+    IEnumerator Load()
+    {
 
         AsyncOperation loading = SceneManager.LoadSceneAsync("Map");
-        
-        while(loading.progress<1){
+
+        while (loading.progress < 1)
+        {
             loading_fill.fillAmount = loading.progress;
             yield return new WaitForEndOfFrame();
         }
     }
 
-    public void StartGame(){
-        quit.gameObject.SetActive(false);
-        start_button.gameObject.SetActive(false);
+    public void StartGame()
+    {
+        buttonsParent.SetActive(false);
         loading_back.gameObject.SetActive(true);
         loading_fill.gameObject.SetActive(true);
         StartCoroutine(Load());
     }
 
-    public void Quit(){
+    public void Quit()
+    {
         Application.Quit();
     }
 }
