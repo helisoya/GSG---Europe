2cc9f134d960e641ad8bc6df4905ea71e363c3e7
helisoya <oudot.julien@laposte.net>
2023-03-03 09:02:56 +0100

    Upgraded remaining Textmeshpro (Except for Events, because of rework)

--- Assets/Scripts/GUI/FocusMenu.cs
+++ Assets/Scripts/GUI/FocusMenu.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FocusMenu : MonoBehaviour
 {
     public GameObject focusMenuRoot;
-    public Text focusText;
+    public TextMeshProUGUI focusText;
     public Transform graphParent;
     public Transform lineParent;
     public GameObject prefabButton;
--- Assets/Scripts/GUI/SettingsMenu.cs
+++ Assets/Scripts/GUI/SettingsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class SettingsMenu : MonoBehaviour
 
     public Toggle sun_t;
 
-    public Dropdown res_d;
+    public TMP_Dropdown res_d;
 
     public Toggle fullscreen_t;
 
@@ -21,71 +22,88 @@ public class SettingsMenu : MonoBehaviour
 
     bool start = true;
 
-    void Awake(){
+    void Awake()
+    {
 
         resolutions = Screen.resolutions;
 
         List<string> names = new List<string>();
-        for(int i = 0;i<resolutions.Length;i++){
-            names.Add(resolutions[i].width+" x "+ resolutions[i].height);
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            names.Add(resolutions[i].width + " x " + resolutions[i].height);
         }
 
         res_d.ClearOptions();
         res_d.AddOptions(names);
 
-        if(PlayerPrefs.HasKey("resolution")){
-            if(PlayerPrefs.GetInt("resolution")<resolutions.Length){
+        if (PlayerPrefs.HasKey("resolution"))
+        {
+            if (PlayerPrefs.GetInt("resolution") < resolutions.Length)
+            {
                 res_d.value = PlayerPrefs.GetInt("resolution");
             }
         }
 
-        if(PlayerPrefs.HasKey("clouds")){
-            cloud_t.isOn = (PlayerPrefs.GetString("clouds")=="true");
+        if (PlayerPrefs.HasKey("clouds"))
+        {
+            cloud_t.isOn = (PlayerPrefs.GetString("clouds") == "true");
         }
-        if(PlayerPrefs.HasKey("sun")){
-            sun_t.isOn = (PlayerPrefs.GetString("sun")=="true");
+        if (PlayerPrefs.HasKey("sun"))
+        {
+            sun_t.isOn = (PlayerPrefs.GetString("sun") == "true");
         }
-        if(PlayerPrefs.HasKey("fullscreen")){
-            fullscreen_t.isOn = (PlayerPrefs.GetString("fullscreen")=="true");
+        if (PlayerPrefs.HasKey("fullscreen"))
+        {
+            fullscreen_t.isOn = (PlayerPrefs.GetString("fullscreen") == "true");
         }
         start = false;
     }
 
-    public void UpdateClouds(){
+    public void UpdateClouds()
+    {
         bool val = cloud_t.isOn;
         clouds.SetActive(val);
-        PlayerPrefs.SetString("clouds",val.ToString());
+        PlayerPrefs.SetString("clouds", val.ToString());
     }
 
-    public void UpdateSun(){
-        if(!sun_t.isOn){
+    public void UpdateSun()
+    {
+        if (!sun_t.isOn)
+        {
             sun.StopCycle();
             sun.enabled = false;
-            PlayerPrefs.SetString("sun",false.ToString());
-        }else{
+            PlayerPrefs.SetString("sun", false.ToString());
+        }
+        else
+        {
             sun.enabled = true;
-            PlayerPrefs.SetString("sun",true.ToString());
+            PlayerPrefs.SetString("sun", true.ToString());
         }
-        
+
     }
 
-    public void UpdateFullscreen(){
-        PlayerPrefs.SetString("fullscreen",fullscreen_t.isOn.ToString());
-        if(!start){
-            Screen.SetResolution(Screen.currentResolution.width,Screen.currentResolution.height,fullscreen_t.isOn);
+    public void UpdateFullscreen()
+    {
+        PlayerPrefs.SetString("fullscreen", fullscreen_t.isOn.ToString());
+        if (!start)
+        {
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, fullscreen_t.isOn);
         }
-        
+
     }
 
-    public void UpdateResolution(){
-        PlayerPrefs.SetInt("resolution",res_d.value);
-        if(!start){
-            Screen.SetResolution(resolutions[res_d.value].width,resolutions[res_d.value].height,fullscreen_t.isOn);
+    public void UpdateResolution()
+    {
+        PlayerPrefs.SetInt("resolution", res_d.value);
+        if (!start)
+        {
+            Screen.SetResolution(resolutions[res_d.value].width, resolutions[res_d.value].height, fullscreen_t.isOn);
         }
-        
+
     }
 
-    public void Quit(){
+    public void Quit()
+    {
         Application.Quit();
     }
 }
